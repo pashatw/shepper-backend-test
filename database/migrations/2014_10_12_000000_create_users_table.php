d0f15bec70aa95a6905a5c093ba884fe7ca8c0d9
Nick Matthews <nwfmatthews@gmail.com>
2020-08-17 15:56:45 +0200

    Adding country code field for users, and updating seeds

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('api_token', 80)->unique()->nullable();
+            $table->string('country_code', 2);
             $table->rememberToken();
             $table->timestamps();
         });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,16 +15,19 @@ class DatabaseSeeder extends Seeder
         factory(User::class)->create([
             'id' => 1,
             'api_token' => 'one',
+            'country_code' => 'GB',
         ]);
 
         factory(User::class)->create([
             'id' => 2,
             'api_token' => 'two',
+            'country_code' => 'FR',
         ]);
 
         factory(User::class)->create([
             'id' => 3,
             'api_token' => 'three',
+            'country_code' => 'DE',
         ]);
     }
 }
